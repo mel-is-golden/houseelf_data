3d23b25118efef5392775c680d86bfb298ac230c
Phillip D. Alderman <phillip.alderman@okstate.edu>
2016-10-10 09:55:16 -0500

    Added function to classify ear size

---
+++ classify_ear_size.R
@@ -0,0 +1,5 @@
+get_size_class <- function(ear_length){
+   # Calculate the size class for one or more earth lengths
+   ear_lengths <- ifelse(ear_length > 10, "large", "small")
+   return(ear_lengths)
+}
